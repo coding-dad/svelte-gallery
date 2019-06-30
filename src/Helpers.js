dd7248b0034f6a50cef62c3961804d3c101775b5
coding-dad <boris.365@outlook.com>
2019-06-30 15:25:19 +0200

    image size reduced

--- src/Helpers.js
+++ src/Helpers.js
@@ -19,16 +19,16 @@
 */
 export const images = [
 	"images/zq-lee-VbDjv8-8ibc-unsplash.jpg?v=1561796309182",
-	"images/krisjanis-mezulis-Ndz3w6MCeWc-unsplash.jpg?v=1561796310554",
-	"images/sepp-rutz-tskqMngoHSA-unsplash.jpg?v=1561796311981",
+	// "images/krisjanis-mezulis-Ndz3w6MCeWc-unsplash.jpg?v=1561796310554",
+	// "images/sepp-rutz-tskqMngoHSA-unsplash.jpg?v=1561796311981",
 	"images/erik-mclean-WhRsHmFtFXQ-unsplash.jpg?v=1561796313563",
-	"images/josh-sorenson-Z33d85enIBI-unsplash.jpg?v=1561796314943",
+	// "images/josh-sorenson-Z33d85enIBI-unsplash.jpg?v=1561796314943",
 	"images/pang-yuhao-wCi28eq8TF4-unsplash.jpg?v=1561796316818",
 	"images/hakon-sataoen-XFiY4FZj8ZE-unsplash.jpg?v=1561796317836",
 	"images/john-fowler-aaIN3y2zcMQ-unsplash.jpg?v=1561796318194",
-	"images/sepp-rutz-hZQOby6ZdIE-unsplash.jpg?v=1561796318251",
-	"images/pascal-habermann-A6hoSqrce5U-unsplash.jpg?v=1561796318397",
-	"images/lance-asper-5Kp5LVjINgI-unsplash.jpg?v=1561796323607",
+	// "images/sepp-rutz-hZQOby6ZdIE-unsplash.jpg?v=1561796318251",
+	// "images/pascal-habermann-A6hoSqrce5U-unsplash.jpg?v=1561796318397",
+	// "images/lance-asper-5Kp5LVjINgI-unsplash.jpg?v=1561796323607",
 	"images/keith-luke-TAm2z1TOges-unsplash.jpg?v=1561796324012"
 ];
 
